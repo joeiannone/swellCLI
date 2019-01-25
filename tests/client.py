fc4f14814413423a1ea696a008970aa1b6038c63
Joseph Iannone <joseph.m.iannone@gmail.com>
2019-01-24 22:37:03 -0500

    client/html parsing

--- tests/client.py
+++ tests/client.py
@@ -2,20 +2,34 @@
 # @Date:   2019-01-24T17:35:01-05:00
 # @Filename: client.py
 # @Last modified by:   Joe Iannone
-# @Last modified time: 2019-01-24T19:53:03-05:00
+# @Last modified time: 2019-01-24T22:36:03-05:00
+
 import sys
 import os.path
 sys.path.insert(0, '..')
 import random
 import json
+import pprint
 from client import Client
 from requestHandler import RequestHandler
 
 request_handler = RequestHandler()
 client = Client(request_handler)
 
-sub_areas = json.loads(client.getSubAreas(client.regions[random.randint(0, len(client.regions)-1)]['ref']))
+region = client.regions[random.randint(0, len(client.regions)-1)]['ref']
+sub_areas = json.loads(client.getSubAreas(region))
+print(region)
 
-local = json.loads(client.getLocalAreas(list(sub_areas.keys())[random.randint(0, len(sub_areas)-1)]));
+sub_area = list(sub_areas.keys())[random.randint(0, len(sub_areas)-1)]
+local_areas = json.loads(client.getLocalAreas(sub_area))
+print(sub_area)
 
-print(json.dumps(local, indent=4, sort_keys=True))
+local_area = list(local_areas.keys())[random.randint(0, len(local_areas)-1)]
+current_conditions = client.getCurrentConditions(local_area)
+print(local_area)
+
+forecast = client.getForecast(local_area)
+
+pprint.pprint(current_conditions)
+
+#pprint.pprint(forecast)
