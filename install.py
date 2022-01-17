57b6604eda8815d7b1acc66c91e39cbddd522730
Joe Iannone <joseph.m.iannone@gmail.com>
2022-01-16 23:55:59 -0500

    check system command exit codes

--- install.py
+++ install.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-# TODO:
-# Needs Testing!!!!!!!!!!
-
 import os, sys
 
 try:
-    os.system('pip install -r requirements.txt')
-    os.system('pyinstaller --onefile main.py')
-    os.system('cp ./dist/main /usr/local/bin/swell')
-    os.system('rm -r build')
-    os.system('rm -r dist')
+  if os.system("pip install -r requirements.txt") != 0:
+    raise Exception("Could not install requirements.")
+  if os.system("pyinstaller --onefile main.py") != 0:
+    raise Exception("Could not create build.")
+  if os.system("sudo cp ./dist/main /usr/local/bin/swell") != 0:
+    raise Exception("Could not publish build.")
+  if os.system("rm -r build dist") != 0:
+    raise Exception("Could not clean up project directory.")
 
 except Exception as e:
-    sys.exit("\nSomething went wrong with install: " + str(e) + "\n")
+  sys.exit("\nSomething went wrong with install: " + str(e) + "\n")
 
 
 sys.exit("\n\nSuccess.\n\nUse the command 'swell' to run.\n\n")
